dbe86ec34935f33b623ef7b3963f0c6617884f3e
Gabriel Yap <gabrielyap37@gmail.com>
2025-08-20 23:55:58 +0800

    Changed name

---
+++ src/main/java/Atlas.java
@@ -0,0 +1,7 @@
+public class Atlas {
+    public static void main(String[] args) {
+        System.out.println("Hello, I'm Atlas.");
+        System.out.println("What can I do for you?");
+        System.out.println("Bye! Hope to see you again soon.");
+    }
+}
