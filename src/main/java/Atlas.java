e214a2c2707397c541238a442b8e656c1741d7e8
Gabriel Yap <gabrielyap37@gmail.com>
2025-08-21 11:28:18 +0800

    Finished Level 1

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,7 +1,23 @@
+import java.util.Scanner;
+
 public class Atlas {
     public static void main(String[] args) {
+        System.out.println("____________________________________________________________");
         System.out.println("Hello, I'm Atlas.");
         System.out.println("What can I do for you?");
-        System.out.println("Bye! Hope to see you again soon.");
+        System.out.println("____________________________________________________________");
+
+        Scanner s = new Scanner(System.in);
+        String s1 = s.nextLine();;
+
+        while (!s1.equals("bye")) {
+            System.out.println("____________________________________________________________");
+            System.out.println(s1);
+            System.out.println("____________________________________________________________");
+            s1 = s.nextLine();
+        }
+        System.out.println("____________________________________________________________");
+        System.out.println("Bye! See you next time :)");
+        System.out.println("____________________________________________________________");
     }
 }
