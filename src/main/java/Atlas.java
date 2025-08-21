b3e2a6dedfb2c8974bab5ca4e019538c77350e46
Gabriel Yap <gabrielyap37@gmail.com>
2025-08-21 16:00:41 +0800

    Add, List

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -1,23 +1,43 @@
 import java.util.Scanner;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Atlas {
     public static void main(String[] args) {
-        System.out.println("____________________________________________________________");
-        System.out.println("Hello, I'm Atlas.");
-        System.out.println("What can I do for you?");
-        System.out.println("____________________________________________________________");
+        List<String> items = new ArrayList<>();
+        Integer c = 0;
+        String space = "     ";
+        String line = "____________________________________________________________";
+        System.out.println(space + line);
+        System.out.println(space + "Hello, I'm Atlas.");
+        System.out.println(space + "What do you want to do today?");
+        System.out.println(space + line);
 
         Scanner s = new Scanner(System.in);
-        String s1 = s.nextLine();;
+        String s1;
 
-        while (!s1.equals("bye")) {
-            System.out.println("____________________________________________________________");
-            System.out.println(s1);
-            System.out.println("____________________________________________________________");
+        while (true) {
             s1 = s.nextLine();
+            if (s1.equals("bye")) {
+                System.out.println(space + line);
+                System.out.println(space + "Bye! See you next time :)");
+                System.out.println(space + line);
+                break;
+            } else if (s1.equals("list")) {
+                System.out.println(space + line);
+                for (int i = 0; i < c; i++) {
+                    System.out.println(space + items.get(i));
+                }
+                System.out.println(space + line);
+            } else {
+                c++;
+                System.out.println(space + line);
+                System.out.println(space + "added: " + s1);
+                System.out.println(space + line);
+                items.add(Integer.toString(c) + ". " + s1);
+            }
+
         }
-        System.out.println("____________________________________________________________");
-        System.out.println("Bye! See you next time :)");
-        System.out.println("____________________________________________________________");
+
     }
 }
