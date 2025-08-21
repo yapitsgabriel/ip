8be194ffaf066c3cf11077cbbaa3bf3e9951629b
Gabriel Yap <gabrielyap37@gmail.com>
2025-08-21 16:19:26 +0800

    Add checklists, mark not working

--- src/main/java/Atlas.java
+++ src/main/java/Atlas.java
@@ -5,7 +5,8 @@ import java.util.List;
 public class Atlas {
     public static void main(String[] args) {
         List<String> items = new ArrayList<>();
-        Integer c = 0;
+        List<Boolean> done = new ArrayList<>();
+        int c = 0;
         String space = "     ";
         String line = "____________________________________________________________";
         System.out.println(space + line);
@@ -26,15 +27,29 @@ public class Atlas {
             } else if (s1.equals("list")) {
                 System.out.println(space + line);
                 for (int i = 0; i < c; i++) {
-                    System.out.println(space + items.get(i));
+                    String num = Integer.toString(i + 1) + ". ";
+                    if (done.get(i)) {
+                        System.out.println(space + num + "[X] " + items.get(i));
+                    } else {
+                        System.out.println(space + num + "[ ] " + items.get(i));
+                    }
                 }
                 System.out.println(space + line);
-            } else {
+            } else if (s1.matches("^mark \\d+$")) {
+                done.set((int) (s1.charAt(5) - '0') - 1, true);
+                System.out.println();
+            } else if (s1.matches("^unmark \\d+$")) {
+                done.set((int) (s1.charAt(5) - '0') - 1, false);
+            }
+            else if (!s1.isEmpty()){
                 c++;
                 System.out.println(space + line);
-                System.out.println(space + "added: " + s1);
+                System.out.println(space + "added: [ ] " + s1);
                 System.out.println(space + line);
-                items.add(Integer.toString(c) + ". " + s1);
+                items.add(s1);
+                done.add(false);
+            } else {
+
             }
 
         }
